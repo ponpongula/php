af45c6e5aa1fc0035581ab48d44be95b9d8e4c62
gula <rqspss1359@gmail.com>
2021-10-25 20:41:06 +0900

    課題０２の追加

--- kadai_02.php
+++ kadai_02.php
@@ -8,4 +8,10 @@ echo "\n";
 $array_month = ["１月","２月","３月","４月","5月","６月","７月","８月","９月","10月","11月","12月"];
 echo $array_month[7];
 echo "\n";
+
+$hello = "Hello, ";
+$name = "masuda";
+$world = "'s World!";
+echo $hello . $name . $world;
+echo "\n";  
 ?>
