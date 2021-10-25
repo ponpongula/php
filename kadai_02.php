753226abcbdaaf389a6492d554647f8b820a80c1
gula <rqspss1359@gmail.com>
2021-10-26 00:39:47 +0900

    追加課題０２

--- kadai_02.php
+++ kadai_02.php
@@ -13,5 +13,30 @@ $hello = "Hello, ";
 $name = "masuda";
 $world = "'s World!";
 echo $hello . $name . $world;
-echo "\n";  
+echo "\n";
+
+$tech_boost = 'tech';
+$tech_boost .= '_boost';
+echo $tech_boost;
+echo "\n";
+
+$a2018_calendar = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"];
+
+
+echo $a2018_calendar["December"];
+echo "\n";
+
+
 ?>
